8da74f475af18262a718845bd5a36ed0cf2ecf9f
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2024-06-03 15:05:42 +0700

    menginput data yang di buat

---
+++ manual execption.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    try {
+        cout << "selamat belajar di prodi TI UMY" << endl;
+        //throw 0.5; //melemparkan sebuah integer maka
+        cout << "pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch (int a) {
+        //blok ini akan dieksekusi
+        cout << "pernyataan akan dieksekusi" << endl;
+    }
+    catch(...) {
+        cout << "default pengecualian dieksekusi" << endl;
+    }
+
+    return 0;
+}
