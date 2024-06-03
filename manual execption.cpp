cc66e8d43b406a7fd170fe63d32407eb95ebcbb5
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2024-06-03 15:10:48 +0700

    menginput hasil dari data array yang di buat

--- manual execption.cpp
+++ manual execption.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main() 
+{
     try {
         cout << "selamat belajar di prodi TI UMY" << endl;
         //throw 0.5; //melemparkan sebuah integer maka
