091925c67c3f4d4a64730c1e3b44867b81a64a12
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2024-06-03 15:22:58 +0700

    mengembalikan bahasa program

--- exceptionstandard.cpp
+++ exceptionstandard.cpp
@@ -10,16 +10,14 @@ int main() {
 		array<int, 3> data = { 3, 5, 7 };
 		//pesan array integer 3 elemen
 		cout << data.at(5) << endl;
-		//memanggil array elemen ke 5
 	}
 	catch (exception& e) {
 		//penangkap menggunakan obyek exection
 		cout << e.what() << endl;
-		/*akan dieksekusi karna array data hanya memiliki 3 elemen*/
+		/*akan dieksekusi karna array data hanya
+         memiliki 3 elemen*/
 	}
 	cout << "baris program yang terakhir" << endl;
 	/*penanda 2: bahasa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
     return 0;
-}
-	
-    
+}    
