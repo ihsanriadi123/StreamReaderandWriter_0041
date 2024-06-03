3305bc42165d850ccc57bd8b7bd072893489f6bb
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2024-06-03 14:51:13 +0700

    menulis file untuk keluar file nya

--- iofilewithpath.cpp
+++ iofilewithpath.cpp
@@ -10,10 +10,35 @@ int main() {
 	cout << "masukan nama file : ";
 	cin >> namafile;
 
-
     //membuka file dalam mode menulis.
 	ofstream outfile;
 	//menunjuk ke sebuah nama file
 	outfile.open(namafile + ".txt" ,ios::out);
+
+    cout << ">= menulis file, \'q\' untuk keluar" << endl;
+
+
+    //unlimited loop untuk menulis
+    while (true) {
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukkan karakter q
+        if (baris == "q") {
+            break;
+        }
+        //menulis  dan memasukkan nilai dari 'baris' ke dalamn file
+        outfile << baris << endl;
+    }
+    //selesai dala menulis sekarang tutup filenya
+    outfile.close();
+   
+
+   //membuka file dalam mode memabaca
+   ifstream infile;
+
+   //menunjuk ke sebuah nama file
+   infile.open(namafile + ".txt", ios::in);
+    
 
 }
